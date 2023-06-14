12c54d29b9a004e7d6815e2a562a696f866ec808
EC2 Default User <ec2-user@ip-172-31-17-93.ap-northeast-1.compute.internal>
2023-06-14 12:42:08 +0000

    トップページ仮

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
- # 顧客用
-# URL /customers/sign_in ...
-devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
  scope module: :public do
     root to: "homes#top"
     get 'about' => 'homes#about'
     resources :posts, only: [:index, :show, :edit, :new, :create, :update, :destroy] do
-      resources :comments, only: [:create, :destroy]
-      resources :favorites, only: [:create, :destroy]
     end
+  end
+ # 顧客用
+# URL /customers/sign_in ...
+devise_for :user,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
 
 # 管理者用
 # URL /admin/sign_in ...
