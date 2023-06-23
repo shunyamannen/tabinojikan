0f65ae25601df9ba8e00bb1b36f76754fabb8dad
EC2 Default User <ec2-user@ip-172-31-17-93.ap-northeast-1.compute.internal>
2023-06-23 08:45:30 +0000

    ゲストログイン機能

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -18,10 +18,33 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+   # ゲスト会員ログイン
+  def guest_sign_in
+    user = User.find_or_create_by!(email: "guest@guest.com") do |guest|
+      guest.name = "ゲストユーザー"
+      guest.password = "aaaaaa"
+    end
+
+    sign_in user
+    redirect_to about_path
+  end
+
+
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+end
+
+ # 退会した会員が、同じアカウントでログイン出来ない処理
+  def reject_withdraw_user
+    @user = User.find_by(name: params[:user][:name])
+    return if !@user
+    if @user.valid_password?(params[:user][:password]) && @user.members_status == true
+      flash[:notice] = "退会済みの為、再登録が必要です。"
+      redirect_to new_user_registration_path
+    end
+  end
 end
